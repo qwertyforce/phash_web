51937c5110ec0c540446cc9b95476270e22cb84c
qwertyforce <qwertyforce@protonmail.com>
2023-01-03 21:17:41 +0300

    Update phash_dedup.py

--- phash_dedup.py
+++ phash_dedup.py
@@ -51,44 +51,55 @@ with DB_phash.begin(buffers=True) as txn:
             all_ids.append(int_from_bytes(key))
 
 
-all_ids = [all_ids[i:i + 512] for i in range(0, len(all_ids), 512)]
-for batch in all_ids:
+all_ids = [all_ids[i:i + 64] for i in range(0, len(all_ids), 64)]
+arr_results = []
+
+for batch in tqdm(all_ids):
     target_features = np.array([index.reconstruct(id) for id in batch if id not in to_remove_id_set])
     if target_features.shape[0] == 0:
         continue
     lims, D, I = index.range_search(target_features, distance_threshold)
     I = [int(el) for el in I]
+    D = [int(el) for el in D]
     for i in range(len(lims)-1):
         if batch[i] in to_remove_id_set:
             continue
         image_ids = I[lims[i]:lims[i+1]]
         image_ids = [el for el in image_ids if el not in to_remove_id_set]
-        if len(image_ids) <= 1:
+        if len(image_ids) <=  1:
             continue
-        print("==============")
-        print(f"similar images: {image_ids}")
-        print(f"distances: {D[lims[i]:lims[i+1]]}")
+        # print("==============")
+        # print(f"similar images: {image_ids}")
+        # print(f"distances: {D[lims[i]:lims[i+1]]}")
         file_names = []
+        resolutions = []
         max_pixels = -1
         id_img_to_keep = -1
-        # file_name_img_to_keep = -1
+
         file_names = [get_file_name_by_id(image_id) for image_id in image_ids]
         for image_id, file_name in zip(image_ids, file_names):
             if not file_name:
+                print("not file_name. Something went wrong!!!")
                 continue
             img_path = IMAGE_PATH+file_name
             width, height = imagesize.get(img_path)
+            resolutions.append([width,height])
             if width*height>max_pixels:
-                max_pixels = width*height
                 id_img_to_keep=image_id
-                # file_name_img_to_keep = file_name
+                max_pixels = width*height
 
         for image_id, file_name in zip(image_ids, file_names):
             if image_id == id_img_to_keep:
                 continue
             to_remove_id_set.add(image_id)
             to_remove_filename_set.add(file_name)
+        arr_results.append([file_names,D[lims[i]:lims[i+1]],resolutions])
 
+print(len(to_remove_id_set))
+
+import json
+with open("phash.res","w") as file:
+    json.dump(arr_results, file)
 
 print("removing duplicates from index")
 index.remove_ids(np.int64(list(to_remove_id_set)))
@@ -103,10 +114,10 @@ with DB_id_to_filename.begin(write=True,buffers=True) as txn:
     for image_id in tqdm(to_remove_id_set):
         txn.delete(image_id)
 
-print("removing duplicates from DB_filename_to_id")   
+print("removing duplicates from DB_filename_to_id and deleting images")   
 with DB_filename_to_id.begin(write=True,buffers=True) as txn:
     for file_name in tqdm(to_remove_filename_set):
-        # remove(IMAGE_PATH+file_name)
+        remove(IMAGE_PATH+file_name)
         txn.delete(file_name.encode())  
 
 print("updating populated.index...")
